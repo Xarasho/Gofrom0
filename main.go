1c4dd4ae52e8b699c939599774aa8487a268a60c
Xarasho <alex.olmedo.rojas@gmail.com>
2024-06-15 09:07:54 -0600

    Next Commit

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Xarasho/Gofrom0/variables"
+	"runtime"
+	// "github.com/Xarasho/Gofrom0/variables"
 )
 
 func main() {
 	// variables.ShowIntegers()
 	// variables.ShowRest()
-	state, text := variables.ConverToText(1588)
-	fmt.Println("Text is: ", text, " and State is: ", state)
+	// state, text := variables.ConverToText(1588)
+	// fmt.Println("Text is: ", text, " and State is: ", state)
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("This isn't Windows, it's: ", os)
+	} else {
+		fmt.Println("This is Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("This is linux")
+	case "darwin":
+		fmt.Println("This is darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
